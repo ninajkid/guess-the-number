085b9f08acb9162c75e125db286e9a2306e39de5
ninajkid <101373051+ninajkid@users.noreply.github.com>
2022-07-21 18:52:15 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,32 @@
+// random value generated
+var y=Math.floor(Math.random()*10+1);
+  
+// counting the number of guesses
+var guess=1;
+playername= localStorage.getItem("player_name");
+// made for correct Guess
+function submit(){
+    var x=document.getElementById("guessField").value;
+    if(x==y){
+        alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN"
+        +guess+"GUESS");
+        guess=1;
+    }
+
+    else if (x>y){
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+    }
+    else{
+        guess++;
+        alert("OOPS SORRY !! TRY A GREATER NUMBER" )
+    }
+}
+
+  
+// function for number guessed by user  
+function playAgain(){
+    y=Math.floor(Math.random()*10+1);
+
+}
+ 
